13acdc27b29a6860457a7bfc064d6737e39efd8c
sakundu <sakundu@ucsd.edu>
2023-05-02 13:07:18 -0700

    Added backend sampling details
    Signed-off-by: sakundu <sakundu@ucsd.edu>

--- sampling/gen_samples.py
+++ sampling/gen_samples.py
@@ -1,15 +1,16 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from typing import List, Optional
+from typing import List, Optional, Tuple
 from skopt.space import Space
 from skopt.sampler import Sobol
 from skopt.sampler import Lhs
 from skopt.sampler import Halton
 
 def gen_lhs_sample(n_samples:int = 16, \
-                space: List[tuple[int or float]] = [(0.4, 2.2), (0.4, 0.9)], \
+                space: List[Tuple[float, float] or Tuple[int, int]] \
+                    = [(0.4, 2.2), (0.4, 0.9)], \
                 criterion:int = 0, lhs_type:int = 1, \
-                seed:int = 42) -> List[tuple[int or float]]:
+                seed:int = 42) -> List[tuple[int or float, int or float]]:
     '''
     n_samples: number of samples  
     space: sample space. If inputs are integer sampled data points will be 
@@ -45,7 +46,7 @@ def gen_sobol_sample(n_samples = 16, space = [(0.4, 2.2), (0.4, 0.9)], \
     samples = sobol.generate(space.dimensions, n_samples, random_state = seed)
     return samples
 
-def plot_sample(data: List[tuple[int or float]], 
+def plot_sample(data: List[Tuple[int or float, int or float]], 
                 label: str,
                 color: str,
                 ax: plt.Axes = None,
