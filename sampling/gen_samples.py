f2231030370f8548d3dfc6a1597cf060c7a3003b
sakundu <sakundu@ucsd.edu>
2023-05-12 21:11:14 -0700

    Added all the sample details
    Signed-off-by: sakundu <sakundu@ucsd.edu>

--- sampling/gen_samples.py
+++ sampling/gen_samples.py
@@ -1,16 +1,17 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from typing import List, Optional, Tuple
+from typing import List, Optional, Tuple, overload, Union
 from skopt.space import Space
 from skopt.sampler import Sobol
 from skopt.sampler import Lhs
 from skopt.sampler import Halton
 
 def gen_lhs_sample(n_samples:int = 16, \
-                space: List[Tuple[float, float] or Tuple[int, int]] \
+                space: List[Union[Tuple[float, float], Tuple[int, int]]] \
                     = [(0.4, 2.2), (0.4, 0.9)], \
                 criterion:int = 0, lhs_type:int = 1, \
-                seed:int = 42) -> List[Tuple[int or float, int or float]]:
+                seed:int = 42) -> Union[List[Tuple[int, int]], \
+                    List[Tuple[float, float]], List[Tuple[int, int, int]]]:
     '''
     n_samples: number of samples  
     space: sample space. If inputs are integer sampled data points will be
